c72571d8bd8276eba84a01a5bdec3e574a82ea6f
Olivia Burge <olivia.burge@gmail.com>
2019-11-11 13:04:47 +1300

    Adding data

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' Rotonuiaha pollen counts
+#'
+#' A dataset containing pollen counts for Rotonuiaha
+#'
+#' @format A data frame with 73 rows and 93 variables:
+#' \describe{
+#'   \item{site}{site name}
+#'   \item{year}{year, derived from radiocarbon dating}
+#'   ...
+#' }
+"rotonuiaha"
